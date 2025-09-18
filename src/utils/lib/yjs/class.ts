dc2c8a1114e93303b1c3f56021d7d0044d04138c
VLADISLAW9 <duplinskij_v_d@mail.ru>
2025-09-18 12:14:33 +0700

    inited y class

---
+++ src/utils/lib/yjs/class.ts
@@ -0,0 +1,26 @@
+import type { AppNode } from '@/utils/types';
+import type { Edge } from '@xyflow/react';
+import * as _Y from 'yjs';
+
+const NODES_KEY = 'NODES_KEY';
+const EDGES_KEY = 'EDGES_KEY';
+
+export class Y {
+  private Doc = new _Y.Doc();
+
+  getNodes() {
+    return this.Doc.getMap().get(NODES_KEY) as AppNode[];
+  }
+
+  getEdges() {
+    return this.Doc.getMap().get(EDGES_KEY) as Edge[];
+  }
+
+  setNodes(value: AppNode[]) {
+    return this.Doc.getMap().get(EDGES_KEY);
+  }
+
+  setEdges(value) {
+    return;
+  }
+}
