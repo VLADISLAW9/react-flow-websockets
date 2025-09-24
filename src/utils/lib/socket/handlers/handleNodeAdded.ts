2405f67fdbc14ca53c1779801d3adf92a23c4214
VLADISLAW9 <duplinskij_v_d@mail.ru>
2025-09-24 11:50:23 +0700

    handler fix

--- src/utils/lib/socket/handlers/handleNodeAdded.ts
+++ src/utils/lib/socket/handlers/handleNodeAdded.ts
@@ -1,8 +1,5 @@
-import { useReactFlowStore } from '@/utils/stores';
 import { reactFlow } from '../../reactFlow/instance';
 
 export const handleNodeAdded = (data: any) => {
-  const { nodes } = useReactFlowStore.getState();
-
-  reactFlow.setNodes([...nodes, data.payload.node]);
+  reactFlow.addNode(data.payload.node);
 };
